2ba9fa33d7a7ea56197b395837bd8371686a55a0
Prashant Gaur <prashantgaur@Prashants-MacBook-Pro.local>
2020-01-18 18:31:29 +0530

    Java Basics

---
+++ JavaBasics/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner takeInput = new Scanner(System.in);
+        System.out.println("Please enter Your Name \r");
+        String name = takeInput.nextLine();
+        System.out.println("Hello " + name);
+        System.out.println("Please Enter an Integer to calculate its Square :\n");
+        int number = takeInput.nextInt();
+        System.out.println("Number entered by " +name+" is "+number);
+        System.out.println("\n Square of "+number +" is " + (number*number));
+        System.out.println("Choose an option to calculate more operation on "+number+" 1. cube\t 2. 60% of it");
+        int choice = takeInput.nextInt();
+
+        if(choice==1){
+            System.out.println("Cube of "+number+" is " +(number*number*number));
+            return;
+        }else if(choice == 2){
+            System.out.println("60% of "+number+" is " +(number*0.60));
+            return;
+        }else{
+            System.out.println("Choose Valid operation ");
+            return;
+        }
+
+
+
+    }
+}
